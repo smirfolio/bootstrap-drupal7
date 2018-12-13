28350caa0f926873b044039be88b0b4f0034b460
smirfolio <ben.sghaier.samir@gmail.com>
2018-12-13 16:58:12 -0500

    Allow disabling the sign up

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -14,12 +14,14 @@
     <div class="md-top-margin">
       <?php print l(t('Forgot your password?'), 'user/password') ?>
       <div>
+        <?php if (empty(variable_get_value('access_signup_button_disabled')) || !variable_get_value('access_signup_button_disabled')): ?>
         <?php $register_url = (module_exists('obiba_agate') ? 'agate' : 'user') . '/register/';
         $option_sign_up = [];
         if (module_exists('obiba_agate')) {
           $option_sign_up = array('fragment' => 'join');
         }
         print l(t('Not a member? Join now'), $register_url, $option_sign_up) ?>
+        <?php endif; ?>
       </div>
     </div>
   </div>
