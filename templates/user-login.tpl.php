c6d3a5afc86efd87dbc948e09e7d91525334db54
smirfolio <ben.sghaier.samir@gmail.com>
2018-12-20 15:45:58 -0500

    Change the enable vs disable signUp Option

--- templates/user-login.tpl.php
+++ templates/user-login.tpl.php
@@ -14,7 +14,7 @@
     <div class="md-top-margin">
       <?php print l(t('Forgot your password?'), 'user/password') ?>
       <div>
-        <?php if (variable_get_value('access_signup_button_enable')): ?>
+        <?php if (empty(variable_get_value('access_signup_button_disabled')) || !variable_get_value('access_signup_button_disabled')): ?>
         <?php $register_url = (module_exists('obiba_agate') ? 'agate' : 'user') . '/register/';
         $option_sign_up = [];
         if (module_exists('obiba_agate')) {
